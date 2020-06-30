980a208568b666d720703aab2ca32261e0d0b5c4
King Wu <king.wuff101@gmail.com>
2020-07-01 00:46:57 +0800

    Refactor

--- store/theme/themeStore.js
+++ store/theme/themeStore.js
@@ -2,15 +2,22 @@ import React, {useReducer, useContext} from 'react';
 export const ThemeContext = React.createContext({})
 export const ThemeProvider = ThemeContext.Provider
 
+export const themeLightMode = 'light'
+export const themeDarkMode = 'dark'
+
 const initState = {
-  mode: 'light'
+  mode: themeLightMode
+}
+
+const actionTypes = {
+  toggle: 'toggle',
 }
 
 const themeReducer = (state, action) => {
   switch (action.type) {
-    case 'toggle':
+    case actionTypes.toggle:
       return {
-        mode: state.mode === 'dark' ? 'light' : 'dark',
+        mode: state.mode === themeLightMode ? themeDarkMode : themeLightMode,
       }
     default:
       throw new Error('Unexpected action');
@@ -25,6 +32,6 @@ export const useThemeContext = () => {
   const [state, dispatch] = useContext(ThemeContext)
   return {
       state: state,
-      onToggleTheme: () => dispatch({type : 'toggle'})
+      onToggleTheme: () => dispatch({type : actionTypes.toggle})
   }
 }
