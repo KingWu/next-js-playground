91e28dbf6afe0fba69950a7e043af54e6fdbb0d4
kw101 <king.wuff101@gmail.com>
2020-06-30 14:23:38 +0800

    Add head into App (#7)

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
+import Head from 'next/head'
 import 'styles/base.css'
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <div className="theme-dark">
+      <Head>
+        <title>Hello World</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </div>
   )
